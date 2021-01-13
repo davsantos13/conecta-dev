4879057126a1c3e6665a47ac0650fe3ab2f77e97
David Santos Evangelista <daavidkarlos@gmail.com>
2021-01-13 17:06:52 -0300

    Finalizando layout tela sign in

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 
 import { makeStyles } from "@material-ui/core/styles";
-import { Typography } from "@material-ui/core";
+import { Grid, Typography, Box, Avatar, Button, Link } from "@material-ui/core";
+import LockOutlinedIcon from "@material-ui/icons/LockOpenOutlined";
+import TextField from "@material-ui/core/TextField";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -9,59 +11,121 @@ const useStyles = makeStyles((theme) => ({
     height: "100vh",
   },
 
-  left: {
-    background: "blue",
-    flexBasis: "58%",
-
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
+  image: {
+    backgroundImage: "url(/images/background.jpg)",
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+    backgroundRepeat: "none",
+    padding: theme.spacing(2),
+    textAlign: "center",
   },
 
-  right: {
-    background: "yellow",
-    flexBasis: "42%",
+  avatar: {
+    marginBottom: theme.spacing(1),
   },
 
+  button: {
+    marginTop: theme.spacing(1),
+  },
   form: {
-    display: "flex",
-    flexDirection: "column",
-    margin: "64px 32px",
-    alignItems: "center",
+    margin: theme.spacing(2, 4),
   },
 }));
+
+function Copyright() {
+  return (
+    <Typography variant="body2" align="center">
+      {"Copyright © "}
+      <a
+        color="inherit"
+        href="https://www.youtube.com/channel/UCVE9-HO_GzLtDK4IGKVSYXA"
+      >
+        Lucas Nhimi
+      </a>{" "}
+      {new Date().getFullYear()}
+    </Typography>
+  );
+}
 
 function SignIn() {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <div className={classes.left}>
-        <Typography style={{ color: "#fff", fontSize: 35, lineHeight: "45px" }}>
-          <strong>Simplificando a forma de conectar desenvolvedores</strong>
+    <Grid container className={classes.root}>
+      <Grid
+        item
+        container
+        direction="column"
+        justify="center"
+        md={7}
+        className={classes.image}
+      >
+        <Typography style={{ color: "#FFF", fontSize: 35, lineHeight: "40px" }}>
+          <strong>Simplificando a forma de conectar desenvolvedores!</strong>
         </Typography>
         <Typography
           variant="body2"
           style={{
-            color: "rgb(255,255,255, 0.7)",
+            color: "rgb(255,255,255, 0.7",
             marginTop: 30,
-            sontsiz: 15,
+            fontSize: 15,
             lineHeight: "30px",
           }}
         >
-          Compartilhe seu conhecimento com toda nossa rede de desenvolvedores
+          Compartilhe seu conhecimento com toda nossa rede de desenvolvedores.
         </Typography>
-      </div>
+      </Grid>
+      <Grid item md={5}>
+        <Box display="flex" flexDirection="column" alignItems="center" mt={8}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography variant="h5">Acesso</Typography>
+          <form className={classes.form}>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="E-mail"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="password"
+              type="password"
+              label="Senha"
+              name="password"
+              autoComplete="current-password"
+            />
+            <Button
+              fullWidth
+              color="primary"
+              variant="contained"
+              className={classes.button}
+            >
+              Entrar
+            </Button>
 
-      <div className={classes.right}>
-        <form className={classes.form}>
-          <h4>Acesso</h4>
-          <input type="text"></input>
-          <input type="text"></input>
-        </form>
-      </div>
-    </div>
+            <Grid container>
+              <Grid item>
+                <Link>Esqueceu sua senha? </Link>
+              </Grid>
+              <Grid item>
+                <Link>Não tem uma conta ? Registre-se</Link>
+              </Grid>
+            </Grid>
+          </form>
+          <Copyright />
+        </Box>
+      </Grid>
+    </Grid>
   );
 }
 
