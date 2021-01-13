c3e0a80738219a5b119d3fde7f1c26fb70a333cb
David Santos Evangelista <devangelista@grupocereal.com.br>
2021-01-13 15:10:46 -0300

    Acrescentando navegação por rotas

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import { ThemeProvider } from "@material-ui/core";
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import theme from './theme';
+import theme from "./theme";
 
 function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <Home/>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/">
+              <Home />
+            </Route>
+            <Route path="/sign-in">
+              <SignIn />
+            </Route>
+            <Route path="/*">
+              <h1>Not Found</h1>
+            </Route>
+          </Routes>
+        </BrowserRouter>
       </ThemeProvider>
-      
     </>
   );
 }
